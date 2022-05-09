74aa1b365ad1159a73ca5ce0ba18f93f30c32197
Jianxi Xu <36491231+JianxiXu@users.noreply.github.com>
2022-05-09 02:21:10 -0700

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -31,7 +31,7 @@ $("#generate").on("click",function(e){
 
 const loadModel = async ()=>{
     console.log("Loading Model")
-    model = await tf.loadLayersModel("https://raw.githubusercontent.com/JianxiXu/JianxiXu.github.io/main/WebModel/model.json")
+    model = await tf.loadLayersModel("https://raw.githubusercontent.com/uragirii/Friends-Generator/master/Website/Models/Model%203/model.json")
     console.log("Model Loaded")
     stopLoading()
 } 
